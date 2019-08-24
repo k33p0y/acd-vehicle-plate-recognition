51046ca9ded1ac0cfefbe717a2eef739d42762ec
NineCloud <gabrielarlo11@gmail.com>
2019-08-24 14:03:04 +0800

    first push

---
+++ localenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ninecloud/DevPortal/plate-number-recognition/localenv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
