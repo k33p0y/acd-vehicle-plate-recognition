988fad0d69d7dcf01b239f86d2e0e40cc6d54f04
NineCloud <gabrielarlo11@gmail.com>
2019-08-27 15:08:21 +0800

    removed cached

--- pnr_sys/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('check-captured/', views.check_captured, name='api-check-captured'),
-    path('live-feed/', views.live_feed, name='api-live-feed'),
-]
--- pnr_sys/vehicle/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'vehicle/index.html')
-
-def test(request):
-    return render(request, 'vehicle/test.html')
