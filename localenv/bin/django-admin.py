ad6cac2c266e58347533899a9157f204b1378f96
NineCloud <gabrielarlo11@gmail.com>
2019-08-27 13:11:04 +0800

    remove cached

--- localenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ninecloud/DevPortal/plate-number-recognition/localenv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- pnr_sys/api/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Latest
-
-admin.site.register(Latest)
